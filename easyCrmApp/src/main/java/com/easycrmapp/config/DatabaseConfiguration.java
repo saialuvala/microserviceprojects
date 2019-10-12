f15c0ca3346ac941db1ceca60d6c1be9fb8f57ad
saialuvala <saialuvala@gmail.com>
2019-10-12 18:48:01 +0530

    first microserivce

---
+++ easyCrmApp/src/main/java/com/easycrmapp/config/DatabaseConfiguration.java
@@ -0,0 +1,34 @@
+package com.easycrmapp.config;
+
+
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+
+@Configuration
+public class DatabaseConfiguration {
+
+	@Bean(name="mySqlDb")
+	@ConfigurationProperties(prefix = "spring.datasource")
+	@Primary
+	public DataSource createDatasourceForCRM() {
+		System.out.println("1.......createDatasourceForCRM");
+		return  (DataSource) DataSourceBuilder.create().build();
+	}
+	
+	@Bean(name="crmJdbcTemplate")
+	@Autowired
+	public JdbcTemplate createJdbcTemplateForCrmApp(@Qualifier("mySqlDb") DataSource ds) {
+		System.out.println("2.......createJdbcTemplateForCrmApp");
+		return new JdbcTemplate(ds);
+	}
+}
---
+++ easyCrmApp/src/main/java/com/easycrmapp/config/EasyCRMApplication.java
@@ -0,0 +1,17 @@
+package com.easycrmapp.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyCRMApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("============EasyCRMApplication===========1");
+		SpringApplication.run(EasyCRMApplication.class, args);
+		System.out.println("============EasyCRMApplication===========2");
+
+	}
+
+}
